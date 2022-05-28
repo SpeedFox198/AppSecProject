7c023dd8bab5dc7a8dbdfe7a389f9b413ea44c35
SpeedFox198 <xstarx125@gmail.com>
2022-05-28 16:24:27 +0800

    lmao

---
+++ init_sql.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+con = sqlite3.connect("database.db")
+
+# Very scary code, resets your database
+# with open("schema.sql") as f:
+#     con.executescript(f.read())
+
+# cur = con.cursor()
+
+# cur.execute("DELETE FROM Users WHERE 1=1")
+# con.commit()
+
+# x = cur.execute("SELECT * FROM Users").fetchall()
+# for i in x:
+#     print(i)
+
+con.commit()
+con.close()
---
+++ schema.sql
@@ -0,0 +1,73 @@
+-- DROP TABLE IF EXISTS Users;
+-- DROP TABLE IF EXISTS Customers;
+-- DROP TABLE IF EXISTS Books;
+-- DROP TABLE IF EXISTS OrderDetails;
+-- DROP TABLE IF EXISTS OrderItems;
+-- DROP TABLE IF EXISTS CartItems;
+-- DROP TABLE IF EXISTS Reviews;
+
+CREATE TABLE Users (
+    user_id TEXT NOT NULL,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    profile_pic TEXT,
+    is_admin INTEGER NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Customers (
+    name TEXT,
+    credit_card_no TEXT,
+    address TEXT,
+    phone_no INTEGER,
+    user_id TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Books (
+    book_id TEXT NOT NULL,
+    language TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    title TEXT NOT NULL,
+    stock INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    author TEXT NOT NULL,
+    description TEXT NOT NULL,
+    cover_img TEXT NOT NULL,
+    PRIMARY KEY (book_id)
+);
+
+CREATE TABLE OrderDetails (
+    order_id TEXT NOT NULL,
+    order_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    shipping_option TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE OrderItems (
+    order_id TEXT NOT NULL,
+    book_id TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES OrderDetails(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+CREATE TABLE CartItems (
+    user_id TEXT NOT NULL,
+    book_id TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
+CREATE TABLE Reviews (
+    book_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
