6fd636667b9f277c77045e1e63431718999139d9
br-1ght <fcw2k16@gmail.com>
2022-05-30 22:33:07 +0800

    some sql stuff for login/signup idk

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -9,3 +9,18 @@ def fetch_db(sql:str, parameters):
             cur = con.cursor()
             data = cur.execute(sql, parameters).fetchall()
             return data
+
+def create_user(user):
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    query = f"""INSERT INTO Users VALUES ({user.id}, {user.username}, {user.email}, {user.password}, NULL, {user.is_admin})"""
+    cur.execute(query)
+    con.close() 
+
+def get_user(form):
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    query = f"""SELECT * FROM Users WHERE username = {form.username.data} and password = {form.username.data}"""
+    user_data = cur.execute(query).fetchone()
+    return user_data
+    con.close()
