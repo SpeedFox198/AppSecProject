2638a16efa12d3560f73e58c16e22b5b8fb979d9
br-1ght <fcw2k16@gmail.com>
2022-06-02 14:31:01 +0800

    admin stuff

--- db_fetch/__init__.py
+++ db_fetch/__init__.py
@@ -21,15 +21,34 @@ def fetch_db(sql: str, parameters):
 def create_user(user):
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
-    query = f"""INSERT INTO Users VALUES ({user.id}, {user.username}, {user.email}, {user.password}, NULL, {user.is_admin})"""
+    query = f"""INSERT INTO Users VALUES ({user.id}, {user.username}, {user.email}, {user.password}, NULL, {user.is_admin});"""
     cur.execute(query)
+    con.commit()
     con.close()
 
 
 def get_user(form):
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
-    query = f"""SELECT * FROM Users WHERE username = {form.username.data} and password = {form.username.data}"""
+    query = f"""SELECT * FROM Users WHERE username = {form.username.data} and password = {form.username.data};"""
     user_data = cur.execute(query).fetchone()
     con.close()
     return user_data
+
+
+def admin_exists():
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    query = f"""SELECT * FROM Users WHERE username = 'admin';"""
+    exist = cur.execute(query).fetchone()
+    con.close()
+    return bool(exist)
+
+
+def create_admin(admin_id, username, email, password):
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    query = f"""INSERT INTO Users VALUES({admin_id}, {username}, {email}, {password}, NULL, 1)"""
+    cur.execute(query)
+    con.commit()
+    con.close()
