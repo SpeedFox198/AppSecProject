480c184c074deffb14a9197da2d5f62e3bf5a23d
SpeedFox198 <xstarx125@gmail.com>
2022-06-15 23:38:39 +0800

    let's hope this works (didn't test lmao)

--- session_handler/__init__.py
+++ session_handler/__init__.py
@@ -1,26 +1,45 @@
 import pickle
 from base64 import b64encode, b64decode
 from hmac import digest, compare_digest
-from itsdangerous import Signer
+from typing import Union
+from os import environ
+
+# Get secret key (you ain't gonna see it in plain text lol)
+SECRET_KEY = environ.get("VERY_SECRET_KEY").encode()
+SEPARATOR = b"."
+
 
 class UserSession:
     def __init__(self, user_id:str, is_admin:bool=False) -> None:
         self.user_id = user_id
         self.is_admin = is_admin
 
+
 def create_user_session(user_id:str, is_admin:bool=False) -> bytes:
-    return b64encode(pickle.dumps(UserSession(user_id, bool(is_admin))))
+    """ Creates and returns user session cookie """
 
-def retrieve_user_session(session:bytes) -> UserSession:
+    # Serialise user session object
+    session = b64encode(pickle.dumps(UserSession(user_id, bool(is_admin))))
 
-    # If no session
-    if session is None:
-        return
-    
-    # Check for integrity of session
-    elif False:
-        return
+    # Generate signature
+    signature = b64encode(digest(SECRET_KEY, session, "sha256"))
 
-    # If session is valid
-    else:
-        return pickle.loads(b64decode(session))
+    # Return user session cookie
+    return session + SEPARATOR + signature
+
+
+def retrieve_user_session(session:bytes) -> Union[UserSession, None]:
+    """ Retrieve user session object from session cookie """
+
+    if session:  # If session is not empty
+
+        # Get session values (session and signature)
+        values = session.split(SEPARATOR)
+        a = digest(SECRET_KEY, values[0], "sha256")
+        b = b64decode(values[-1])
+
+        # Check for integrity of session
+        if compare_digest(a, b):
+
+            # Returns user session object if session is valid
+            return pickle.loads(b64decode(values[0]))
