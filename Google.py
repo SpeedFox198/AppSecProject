cbd66161fa63efeb29b019b90463fbc4a61cbb80
MikuChanUwU <57163606+MikuChanUwU@users.noreply.github.com>
2022-06-22 13:41:50 +0800

    WIP Gmail API

--- Google.py
+++ Google.py
@@ -7,7 +7,6 @@ import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
 def gmail_create_draft():
     """Create and insert a draft email.
        Print the returned draft's message and id.
@@ -49,6 +48,43 @@ def gmail_create_draft():
 
     return draft
 
+def gmail_send_message():
+    """Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    creds, _ = google.auth.default()
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        message = EmailMessage()
+
+        message.set_content('This is automated draft mail')
+
+        message['To'] = 'gduser1@workspacesamples.dev'
+        message['From'] = 'gduser2@workspacesamples.dev'
+        message['Subject'] = 'Automated draft'
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+            .decode()
+
+        create_message = {
+            'raw': encoded_message
+        }
+        # pylint: disable=E1101
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+    return send_message
 
 if __name__ == '__main__':
     gmail_create_draft()
+    gmail_send_message()
