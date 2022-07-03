93bd66ad2af9503e6f548243c1cfe35918328d2b
SpeedFox198 <xstarx125@gmail.com>
2022-07-03 23:56:39 +0800

    change

--- db_fetch/review.py
+++ db_fetch/review.py
@@ -25,9 +25,3 @@ def add_review(book_id: str, user_id: str, stars: int, content: str) -> None:
         [book_id, user_id, stars, content],
         ["book_id", "user_id", "stars", "content"]
     )
-
-if __name__ == "__main__":
-    testUser = "54dd57bf-8b8e-5cc1-af95-14478ca84b21"
-    MikuBlyat = "5764d848-a0dc-5857-b4fd-31102dc764dc"
-    book = "19a4cc17-117a-4a20-8ad6-cc3c243e68a7"
-    add_review(book, MikuBlyat, 0, "Horrible book, I don't recommend this, aa, itsumo no you ni sugiru hibi ni akubi ga deru sanzameku yoru, koe, kyou mo shibuya no machi ni asa ga furu dokoka munashii you na sonna kimochi tsumaranai na demo sore de ii sonna mon sa kore de ii")
