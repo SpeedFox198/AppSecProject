7d0be4c8edc8ba8406926377458d1fdfc775c0cb
SpeedFox198 <xstarx125@gmail.com>
2022-07-03 23:55:40 +0800

    review section (WIP) 80%

--- __init__.py
+++ __init__.py
@@ -1660,17 +1660,17 @@ def api_reviews(book_id):
 # Error handling page
 @app.errorhandler(403)
 def forbidden(e):
-    return render_template("error/403.html")
+    return render_template("error/403.html"), 403
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("error/404.html")
+    return render_template("error/404.html"), 404
 
 
 @app.errorhandler(429)
 def too_many_request(e):
-    return render_template("error/429.html")
+    return render_template("error/429.html"), 429
 
 
 """    Main    """
--- static/js/book_info.js
+++ static/js/book_info.js
@@ -1,6 +1,87 @@
-// Get all forms that needs validation
-const forms = document.querySelectorAll('.needs-validation');
-
-// Iterate through forms and prevent submission
-Array.prototype.filter.call(forms, form => {
+function createElementClass(elementName, className) {
+    const e = document.createElement(elementName);
+    if (className) e.className = className;
+    return e
 }
+
+function createDiv(className) {
+    return createElementClass("div", className);
+}
+
+function createPfp(profilePic, username) {
+    const colDiv = createDiv("col-1 profile-pic-col");
+    const imgWrapper = createDiv("img-wrapper img-1-1");
+    const img = createElementClass("img", "profile-pic");
+    img.src = profilePic;
+    img.alt = username;
+    imgWrapper.appendChild(img);
+    colDiv.appendChild(imgWrapper);
+    return colDiv;
+}
+
+function createRatings(stars) {
+    const theDiv = createDiv("review-stars");
+    for (let i=0; i < stars; i++) {
+        let theStar = createElementClass("i", "fas fa-star");
+        theDiv.appendChild(theStar);
+    }
+    for (let i=stars; i < 5; i++) {
+        let theStar = createElementClass("i", "far fa-star");
+        theDiv.appendChild(theStar);
+    }
+    return theDiv
+}
+
+function createDetails(username, stars, time, content) {
+    const colDiv = createDiv("col-11");
+    const usernameDiv = createDiv("");
+    const starsDiv = createRatings(stars);
+    const timeDiv = createDiv("mb-2");
+    const contentDiv = createDiv("");
+    usernameDiv.textContent = username;
+    timeDiv.textContent = time;
+    contentDiv.textContent = content;
+    colDiv.appendChild(usernameDiv);
+    colDiv.appendChild(starsDiv);
+    colDiv.appendChild(timeDiv);
+    colDiv.appendChild(contentDiv);
+    return colDiv
+}
+
+function createReview(review) {
+    let {content, profile_pic, stars, time, username} = review;
+    const reviewDiv = createDiv("reviews row p-3");
+    const pfpDiv = createPfp(profile_pic, username);
+    const detailsDiv = createDetails(username, stars, time, content);
+    const hrLine = createElementClass("hr", "mx-3");
+    reviewDiv.appendChild(pfpDiv);
+    reviewDiv.appendChild(detailsDiv);
+    return reviewDiv;
+}
+
+function displayReviews(customerReviews, reviews) {
+    for(let i=0; i < reviews.length; i++) {
+        let reviewElement = createReview(reviews[i])
+        let line = createElementClass("hr", "mx-3");
+        customerReviews.appendChild(reviewElement);
+        customerReviews.appendChild(line);
+    }
+}
+
+function noReviews(customerReviews) {
+    customerReviews.textContent = "No reviews have been written for this book.";
+}
+
+(async () => {
+    const customerReviews = document.getElementById("customerReviews");
+    const url = "/api/reviews/" + window.location.pathname.split("/")[2]
+    const response = await fetch(url);
+    const reviews = await response.json();
+
+    if (reviews) {
+        displayReviews(customerReviews, reviews);
+    }
+    else {
+        noReviews(customerReviews);
+    }
+})();
