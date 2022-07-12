bbbdcb191dbc3dfdc807c11fb4557934a8b4b279
SpeedFox198 <xstarx125@gmail.com>
2022-07-12 09:59:28 +0800

    fixed the bug (hopefully)

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-from inspect import CO_ASYNC_GENERATOR
 from flask import (
     Flask, render_template, request, redirect, url_for, flash,
     make_response, g as flask_global, abort, jsonify, session  # TODO: session to be removed
@@ -73,21 +72,21 @@ def get_user():
             return User(*user_data)
 
 
-def add_cookie(cookies:dict, x):
+def add_cookie(cookies:dict):
     """ Adds cookies """
     if not isinstance(cookies, dict):
         raise TypeError("Expected dictionary")
-    new_cookies:dict = x.get(NEW_COOKIES, default={})
+    new_cookies:dict = flask_global.get(NEW_COOKIES, default={})
     new_cookies.update(cookies)
-    x[NEW_COOKIES] = new_cookies
+    flask_global.new_cookies = new_cookies
 
-def remove_cookies(cookies:list, flask_global):
+def remove_cookies(cookies:list):
     """ Remove cookies """
     if not isinstance(cookies, list):
         raise TypeError("Expected list")
     expired_cookies:list = flask_global.get(EXPIRED_COOKIES, default=[])
     expired_cookies.extend(cookies)
-    flask_global[EXPIRED_COOKIES] = expired_cookies
+    flask_global.expired_cookies = expired_cookies
 
 
 """ Before first request """
@@ -219,9 +218,9 @@ def sign_up():
             flash("Password cannot contain username", "sign-up-password-error")
             return render_template("user/sign_up.html", form=sign_up_form)
 
-        add_cookie({"username": username}, flask_global)
-        add_cookie({"email": email}, flask_global)
-        add_cookie({"password": password}, flask_global)
+        add_cookie({"username": username})
+        add_cookie({"email": email})
+        add_cookie({"password": password})
 
         oneTimePass = generateOTP()
         print(oneTimePass)
@@ -230,7 +229,7 @@ def sign_up():
         message = "Do not reply to this email.\nPlease enter " + oneTimePass + " as your OTP to complete your registration."
 
         gmail_send(email, subject, message)
-        add_cookie({"OTP": oneTimePass}, flask_global)
+        add_cookie({"OTP": oneTimePass})
         return redirect(url_for("OTP"))
 
     # Render sign up page
